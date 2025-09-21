524c93c496cbb739a0b7147db2f1311c9eb928d9
diogo <alvarwngadiogo228@gmail.com>
2025-09-20 18:37:12 -0700

    commit

--- produtos/src/main/java/br/com/dobackaofront/produtos/controller/CursoController.java
+++ produtos/src/main/java/br/com/dobackaofront/produtos/controller/CursoController.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,34 +36,92 @@ public class CursoController {
 		this.cursoService = cursoService;
 	}
 	
+	
+	/*			MATERIAL DE ESTUDO:
+	 * 
+	 * RESPONSE ENTITY é usado em protocolos http
+	 * 
+		O que é Response Entity?
+	No Spring Boot, o ResponseEntity é uma 
+	classe que representa toda a resposta HTTP. 
+	Ela te dá controle completo sobre o que você 
+	envia de volta ao cliente, incluindo o corpo da 
+	resposta, os cabeçalhos HTTP e o código de status 
+	(como 200 OK, 404 Not Found, 500 Internal Server 
+	Error, etc.).*/
 	@GetMapping
 	public ResponseEntity<List<Curso>> listarTodosCursos(){
 		List<Curso> cursos = cursoService.listarTodosCursos();
 		return ResponseEntity.ok(cursos);
 		//linha acima diz que a requisição deu certo
 		
-		/*MATERIAL DE ESTUDO:
-		 * 
-		 * Response entity é usado em protocolos http
-			O que é Response Entity?
-		No Spring Boot, o ResponseEntity é uma 
-		classe que representa toda a resposta HTTP. 
-		Ela te dá controle completo sobre o que você 
-		envia de volta ao cliente, incluindo o corpo da 
-		resposta, os cabeçalhos HTTP e o código de status 
-		(como 200 OK, 404 Not Found, 500 Internal Server 
-		Error, etc.).*/
+
 	}
 	
-	@GetMapping
+	
+	/*o pathvariable pega o que esta na rota,
+	 * por exemplo:
+	 * api/cursos/1
+	 * ele pega o 1*/
+	
+	@GetMapping("/{id}")
 	public ResponseEntity<Curso> buscarCursoPorId(@PathVariable Long id){
 		Optional<Curso> curso = cursoService.buscarCursoPorId(id);
 		return curso.map(ResponseEntity::ok)
 				.orElse(ResponseEntity.notFound().build());
-		/*o pathvariable pega o que esta na rota,
-		 * por exemplo:
-		 * api/cursos/1
-		 * ele pega o 1*/
+
+		
+	}
+	
+	
+	/*O front enviará uma requisição post(JSON), e quando chegar aqui(METODO ABAIXO),
+	 * ele será transformado em objeto
+	 * 
+	 *  MATERIAL DE ESTUDO
+	 *  O @RequestBody no Spring Boot (e em outros 
+	 *  frameworks Java) faz exatamente isso: ele pega 
+	 *  o corpo da requisição HTTP, que geralmente é um 
+	 *  JSON ou XML, e automaticamente o converte em 
+	 *  um objeto Java.
+
+		Esse processo é chamado de deserialização. Ele 
+		é fundamental para que você possa receber dados 
+		de um cliente (como um navegador ou um aplicativo 
+		móvel) e trabalhar com eles de forma simples e orientada 
+		a objetos no seu código.*/
+	
+	@PostMapping
+	public ResponseEntity<Curso> adicionarCurso(@RequestBody Curso curso){
+
+		Curso novoCurso = cursoService.salvarCurso(curso);
+		return ResponseEntity.status(HttpStatus.CREATED).body(novoCurso);
+		
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Curso> atualizarCurso(@PathVariable Long id, @RequestBody Curso cursoAtualizado){
+		Optional<Curso> curso = cursoService.buscarCursoPorId(id);
+		
+		//se o objeto curso esta presente(se existir)
+		if(curso.isPresent()) {
+			cursoAtualizado.setId(id);//o objeto nao possui id(pois sao apenas os dados novos, o id vem do auto incremento, entao nao da para atualizar), entao aqui eu coloco nele
+			Curso cursoAtualizadoSalvo = cursoService.salvarCurso(cursoAtualizado);//salvo o objeto com o id
+			return ResponseEntity.ok(cursoAtualizado);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> excluirCurso(@PathVariable Long id){
+		Optional<Curso> curso = cursoService.buscarCursoPorId(id);
+		
+		if(curso.isPresent()) {
+			cursoService.excluirCurso(id);
+			return ResponseEntity.noContent().build();
+		}else {
+			return ResponseEntity.notFound().build();
+		}
 		
 	}
 }
